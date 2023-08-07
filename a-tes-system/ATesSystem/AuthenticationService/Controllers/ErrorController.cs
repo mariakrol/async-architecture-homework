710bda4a856f06d9a57169fc2d7e0b6337db1d7a
mkrol <krol.mariya@gmail.com>
2023-08-07 22:37:44 +0200

    Add error controller

---
+++ a-tes-system/ATesSystem/AuthenticationService/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorController : Controller
+{
+    [Route("/error")]
+    public IActionResult HandleError() => Problem();
+
+    [HttpGet("throw")]
+    public IActionResult Throw() => throw new Exception("Sample exception.");
+}
--- a-tes-system/ATesSystem/AuthenticationService/Startup.cs
+++ a-tes-system/ATesSystem/AuthenticationService/Startup.cs
@@ -52,6 +52,15 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/error");
+        }
+
         app.UseRouting();
         app.UseHttpsRedirection();
 
