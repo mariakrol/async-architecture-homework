496d7ed46aad801362ec274f4298f19b6fd7c12e
mkrol <krol.mariya@gmail.com>
2023-08-13 21:58:38 +0200

    Remove redundant service

--- a-tes-system/ATesSystem/AuthenticationService/Queue/Event/MessageQueueEventBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuthenticationService.Queue.Event;
-
-public abstract class MessageQueueEventBase<TPayload>
-{
-    public abstract string EventName { get; }
-
-    public abstract TPayload Payload { get; }
-}
